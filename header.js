1c971ec3f3f23bd3e07feee6600e5505b4395e37
RabotnikGoda <61095349+RabotnikGoda@users.noreply.github.com>
2021-05-27 16:14:00 +0300

    create basket

--- header.js
+++ header.js
@@ -3,9 +3,27 @@ $(function () {
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 1) {
-            header.addClass('header-fixed');
+            header.addClass('header-sticky');
         } else {
-            header.removeClass('header-fixed');
+            header.removeClass('header-sticky');
+        }
+    });
+});
+$(function () {
+    let header = $('.header-menu');
+    let hederHeight = header.height();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            header.addClass('header-sticky');
+            $('body').css({
+                'paddingTop': hederHeight + 'px'
+            });
+        } else {
+            header.removeClass('header-sticky');
+            $('body').css({
+                'paddingTop': 0
+            })
         }
     });
 });
