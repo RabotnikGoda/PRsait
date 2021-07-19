92d69ddc15155cb2a6c061e9cf5841b3e1940981
danil <rabotnikgoda2767@gmail.com>
2021-07-19 14:48:34 +0300

    add sort!

--- datass.js
+++ datass.js
@@ -1,109 +1,121 @@
 var data = {
-    "cards": [
-        {
-            "img": "images/img-proj/in16.jpg",
-            "name": "Торт Ежовое счастье",
-            "disPrice": 990,
-            "price": "1230  р./кг",
-            "weight": "3,5",
-            "wiews": 1400
-        },
-        {
-            "img": "images/img-proj/2504_large.jpg",
-            "name": "Торт Ягодные будни",
-            "disPrice": "1470",
-            "price": "",
-            "weight": "2",
-            "wiews": "1560"
-        },
-        {
-            "img": "images/img-proj/265.jpg",
-            "name": "Торт Рождественские будни",
-            "disPrice": 991,
-            "price": "1200  р./кг",
-            "weight": "3",
-            "wiews": "961"
-        },
-        {
-            "img": "images/img-proj/2512_large.jpg",
-            "name": "Торт Ягодный коктейль",
-            "disPrice": "1300",
-            "price": " ",
-            "weight": "2",
-            "wiews": "2160"
-        },
-        {
-            "img": "images/img-proj/2515_large.jpg",
-            "name": "Торт Ягодный рассвет",
-            "disPrice": "1390",
-            "price": " ",
-            "weight": "2",
-            "wiews": "1433"
-        },
-        {
-            "img": "images/img-proj/id100550.jpg",
-            "name": "Торт Ягоды и белый шоколад",
-            "disPrice": "1460",
-            "price": " ",
-            "weight": "4",
-            "wiews": "1690"
-        },
-        {
-            "img": "images/img-proj/id100554.jpg",
-            "name": "Торт Ягодный тандем",
-            "disPrice": "1340",
-            "price": "1460 р./кг",
-            "weight": "3",
-            "wiews": "1560"
-        },
-        {
-            "img": "images/img-proj/pupa.jpg",
-            "name": "Торт Ведьмино вариво",
-            "disPrice": "1560",
-            "price": " ",
-            "weight": "3",
-            "wiews": "1300"
-        },
-        {
-            "img": "images/img-proj/ng-shary.jpg",
-            "name": "Торт Святящиеся шары",
-            "disPrice": "1200",
-            "price": "1450 р./кг",
-            "weight": "2",
-            "wiews": "1560"
-        },
-        {
-            "img": "images/img-proj/uini109.jpg",
-            "name": "Торт Ягодный фреш",
-            "disPrice": "1200",
-            "price": " ",
-            "weight": "1",
-            "wiews": "1507"
-        },
-        {
-            "img": "images/img-proj/untitled-1082.jpg",
-            "name": "Торт Янтарый осколок",
-            "disPrice": "1240",
-            "price": "1580 р./кг",
-            "weight": "3",
-            "wiews": "2109"
-        },
-        {
-            "img": "images/img-proj/untitled-2259.jpg",
-            "name": "Торт ягодный шик",
-            "disPrice": "1433",
-            "price": " ",
-            "weight": "1",
-            "wiews": "1700"
-        }
-    ]
-}
+  id: "",
+  cards: [
+    {
+      id: "1",
+      img: "images/img-proj/in16.jpg",
+      name: "Торт Ежовое счастье",
+      disPrice: 990,
+      price: "1230  р./кг",
+      weight: "3,5",
+      wiews: 1400,
+    },
+    {
+      id: "10",
+      img: "images/img-proj/2504_large.jpg",
+      name: "Торт Ягодные будни",
+      disPrice: "1470",
+      price: "",
+      weight: "2",
+      wiews: "1560",
+    },
+    {
+      id: "3",
+      img: "images/img-proj/265.jpg",
+      name: "Торт Рождественские будни",
+      disPrice: 991,
+      price: "1200  р./кг",
+      weight: "3",
+      wiews: "961",
+    },
+    {
+      id: "4",
+      img: "images/img-proj/2512_large.jpg",
+      name: "Торт Ягодный коктейль",
+      disPrice: "1300",
+      price: " ",
+      weight: "2",
+      wiews: "2160",
+    },
+    {
+      id: "5",
+      img: "images/img-proj/2515_large.jpg",
+      name: "Торт Ягодный рассвет",
+      disPrice: "1390",
+      price: " ",
+      weight: "2",
+      wiews: "1433",
+    },
+    {
+      id: "6",
+      img: "images/img-proj/id100550.jpg",
+      name: "Торт Ягоды и белый шоколад",
+      disPrice: "1460",
+      price: " ",
+      weight: "4",
+      wiews: "1690",
+    },
+    {
+      id: "7",
+      img: "images/img-proj/id100554.jpg",
+      name: "Торт Ягодный тандем",
+      disPrice: "1340",
+      price: "1460 р./кг",
+      weight: "3",
+      wiews: "1560",
+    },
+    {
+      id: "8",
+      img: "images/img-proj/pupa.jpg",
+      name: "Торт Ведьмино вариво",
+      disPrice: "1560",
+      price: " ",
+      weight: "3",
+      wiews: "1300",
+    },
+    {
+      id: "9",
+      img: "images/img-proj/ng-shary.jpg",
+      name: "Торт Святящиеся шары",
+      disPrice: "1200",
+      price: "1450 р./кг",
+      weight: "2",
+      wiews: "1560",
+    },
+    {
+      id: "2",
+      img: "images/img-proj/uini109.jpg",
+      name: "Торт Ягодный фреш",
+      disPrice: "1200",
+      price: " ",
+      weight: "1",
+      wiews: "1507",
+    },
+    {
+      id: "11",
+      img: "images/img-proj/untitled-1082.jpg",
+      name: "Торт Янтарый осколок",
+      disPrice: "1240",
+      price: "1580 р./кг",
+      weight: "3",
+      wiews: "2109",
+    },
+    {
+      id: "12",
+      img: "images/img-proj/untitled-2259.jpg",
+      name: "Торт ягодный шик",
+      disPrice: "1433",
+      price: " ",
+      weight: "1",
+      wiews: "1700",
+    },
+  ],
+};
 
-
-// data = {
+// data = { id: "",
 //     "version": "0.0.16",
 //     "menu": [
-//         {
+//         { id: "",
 //             "name": "Овощной",
 //             "description": "Соус и овощи на выбор",
 //             "image": "i/sandwiches/ovoshnoy.png",
@@ -112,9 +124,9 @@ var data = {
 //             "market": "subway",
 //             "type": "multiple",
 //             "weight": 1,
-//             "components": {
-//                 "size": {},
+//             "components": { id: "",
+//                 "size": { id: "",},
 //                 "bread": "whit-italian",
 //                 "vegetable": [],
 //                 "sauce": [],
-                // "filling": []
+// "filling": []
